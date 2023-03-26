71fe48e15583c1a7cf394141db964f81da3246c5
tristangclvs <goncalvestristan.pro@pm.me>
2023-03-26 15:21:10 +0200

    Flask displays genre
    TODO:
    - place the mp3 to wav
    - split audio to 30 secs extracts and predict on all of them to have the most precise prediction

--- myApp.py
+++ myApp.py
@@ -1,176 +1,191 @@
+# Flask imports
+import math
+
 from flask import *
-import json
-import datetime
-import openai
+from flask_wtf import FlaskForm
+from wtforms import FileField
+from wtforms.validators import DataRequired
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    current_page = "home"
-    return render_template('home.html', current_page=current_page)
-
-
-@app.route('/palette', methods=['GET', 'POST'])
-def palette():
-    current_page = "Your palette"
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        if not prompt:
-            flash('Prompt is required!')
-            res = make_response(redirect(url_for('index')))
-        else:
-            colors = [prompt, getpalette(prompt)]
-
-            L = get_luminance(colors[1])  # TEST ICI
-            contrasts = find_contrasts(L)
-
-            colors.append(contrasts)
-
-            palettes, expire_date = setCookies()
-            palettes.append(colors)
-            res = make_response(render_template('palette.html', colors=colors,
-                                prompt=prompt, current_page=current_page))  # ENLEVER LUMI SI MARCHE PAS
-            res.set_cookie('palettes', json.dumps(
-                palettes, indent=2), expires=expire_date)
-    else:
-        res = make_response(redirect(url_for('index')))
-    return res
+# Other imports
+import tensorflow as tf
+import numpy as np
+import librosa
+import librosa.display
+import audioread
+import os
+# =============================================================================
 
 
-@app.route("/contact")
-def contact():
-    current_page = "contact"
-    return render_template('contact.html', current_page=current_page)
+# ======================================
+# CONSTANTS
+data_empty = {
+    "mapping": [
+        "blues",
+        "classical",
+        "country",
+        "disco",
+        "hiphop",
+        "jazz",
+        "metal",
+        "pop",
+        "reggae",
+        "rock"
+    ],
+    "mfcc": [],
+    "labels": []
+}
+
+sr = 22500
+duration = 30  # duration in seconds
+samples_per_track = sr * duration
+num_segments = 6
+hop_length = 512
+
+num_samples_per_segment = int(samples_per_track / num_segments)
+expected_num_mfcc_vectors_per_segment = math.ceil(
+    num_samples_per_segment / hop_length)
+
+# ======================================
+most_common = lambda lst: max(set(lst), key=lst.count)
+# =============================================================================
+def load_model():
+    model_version = ""
+    while model_version not in ["6", "10", "11", "12"]:
+        model_version = input("Select model version (6, 10 - 12):  ")
+    model = tf.keras.models.load_model(f"./static/models/cnn_v{model_version}")
+    return model
 
 
-def getpalette(prompt_text):
-    # input_text is the one given to openAi
-    input_text = 'Elaborate a palette of 5 colors based on the sentiment analysis of this prompt: ' + prompt_text + \
-        ' Write the answer as followed: "hexadecimal value$Name of the color$One sentiment the color is based on$Detailed and contextualized description of the sentiment."'
-    # connection to the openAI API using Joseph's credentials
-    openai.api_key = "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"
-    response = openai.Completion.create(model="text-davinci-003",
-                                        prompt=input_text,
-                                        temperature=0.2,
-                                        max_tokens=500)  # we define a maximum of 0.01$ per input
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'testflaskapp'
+    model = load_model()
+    print('Model loaded. ')
+    print(model.summary())
 
-    # If response is null then modify the code to return NotFound or problem....
+    # Clean the uploads folder on startup
+    for file in os.listdir('./static/uploads'):
+        os.remove('./static/uploads/' + file)
 
-    # collecting the answer and selecting the important informations out of it
-    vals = response["choices"][0]["text"].split('.')
-    colors = []
-    if len(vals) == 6 or vals[-1] == '':
-        vals.pop()
-    for i in range(5):
-        # colors is a list of 5 list of 1. color 2. color name 3. sentiment 4. sentiment analysis
-        colors.append(vals[i].split('$'))
-    for inner in colors:
-        for i in range(len(inner)):
-            inner[i] = inner[i].replace('\n', '')
-    return colors
+    class UploadForm(FlaskForm):
+        file = FileField('File', validators=[DataRequired()])
+        # FileRequired(),FileAllowed(images, 'Images only!')
 
+    @app.route('/upload', methods=['GET', 'POST'])
+    def upload():
+        form = UploadForm()
 
-def setCookies():
-    palettes = []
-    if 'palettes' in request.cookies:
-        palettes = json.loads(request.cookies.get('palettes'))
-        expire_date = datetime.datetime.strptime(
-            palettes[0], "%m/%d/%Y").date()
-        if len(palettes[1:]) == 3:
-            palettes.pop(1)
-    else:
-        expire_date = datetime.datetime.now()
-        expire_date = expire_date + datetime.timedelta(days=30)
-        palettes.append(expire_date.strftime("%m/%d/%Y"))
+        if form.validate_on_submit():
+            file = request.files['file']
+            print(f"User selected file: {file.filename}")  # print a message
+            file.save('./static/uploads/' + file.filename)  # save the file to the uploads folder
+            print("File saved successfully")
 
-    return palettes, expire_date
+            file_path = './static/uploads/' + file.filename
+            audio_file = audioread.audio_open(file_path)
 
+            file_mfcc(file_path, num_samples_per_segment=num_samples_per_segment,
+                      expected_num_mfcc_vectors_per_segment=expected_num_mfcc_vectors_per_segment,
+                      dirpath=None,
+                      data=data_empty,
+                      hop_length=512, num_segments=num_segments, n_mfcc=25, n_fft=2048,
+                      iterator=1,
+                      file_duration=audio_file.duration)
+            inputs = np.array(data_empty["mfcc"])
+            targets = np.array(data_empty["labels"])
 
-def rgbtorange01(color):
-    """R, G or B value put to range 0-1"""
-    return color / 255
+            predicted_genre = predict_sample(model, data_empty, inputs)
+            result_html = result(predicted_genre)
+            # return redirect(url_for('result', predicted_genre=predicted_genre))
+            return result_html
+        return render_template('upload.html', form=form)
 
+    @app.route("/")
+    def index():
+        current_page = "home"
+        return render_template('home.html', current_page=current_page)
 
-def get_luminance(colors):
-    L = []
-    for color in colors:
-        r_s_rgb, g_s_rgb, b_s_rgb = hex_to_rgb(color[0])
-        r_s_rgb, g_s_rgb, b_s_rgb = rgbtorange01(
-            r_s_rgb), rgbtorange01(g_s_rgb), rgbtorange01(b_s_rgb)
-
-        # if RsRGB <= 0.03928 then R = RsRGB/12.92 else R = ((RsRGB+0.055)/1.055) ^ 2.4
-        # if GsRGB <= 0.03928 then G = GsRGB/12.92 else G = ((GsRGB+0.055)/1.055) ^ 2.4
-        # if BsRGB <= 0.03928 then B = BsRGB/12.92 else B = ((BsRGB+0.055)/1.055) ^ 2.4
-
-        if r_s_rgb <= 0.3928:
-            R = r_s_rgb / 12.92
-            G = ((g_s_rgb + 0.055) / 1.055) ** 2.4
-            B = ((b_s_rgb + 0.055) / 1.055) ** 2.4
-        elif g_s_rgb <= 0.03928:
-            R = ((r_s_rgb + 0.055) / 1.055) ** 2.4
-            G = g_s_rgb / 12.92
-            B = ((b_s_rgb + 0.055) / 1.055) ** 2.4
-        elif b_s_rgb <= 0.03928:
-            R = ((r_s_rgb + 0.055) / 1.055) ** 2.4
-            G = ((g_s_rgb + 0.055) / 1.055) ** 2.4
-            B = b_s_rgb / 12.92
-        else:
-            R = ((r_s_rgb + 0.055) / 1.055) ** 2.4
-            G = ((g_s_rgb + 0.055) / 1.055) ** 2.4
-            B = ((b_s_rgb + 0.055) / 1.055) ** 2.4
-
-        L.append(0.2126 * R + 0.7152 * G + 0.0722 * B)
-    return L
+    @app.route("/result")
+    def result(predicted_genre):
+        current_page = "result"
+        return render_template('result.html', predicted_genre=predicted_genre, current_page=current_page)
 
 
-def find_contrasts(luminance_array):
-    contrasts = []
-    for luminance in luminance_array:
-        if luminance <= 0.18:
-            contrasts.append('#fff')
-        else:
-            contrasts.append('#000')
-    return contrasts
+    return app
 
 
-def hex_to_rgb(value):
-    value = value.lstrip(' #')
-    lv = len(value)
-    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+def predict(model, x, data, y="Not given"):
+    x = x[np.newaxis, ...]
+    prediction = model.predict(x)
+    # print(prediction)
+    # for i in range(len(prediction)):
+    #     print("Genres: {}, Percentage: {}%".format(data["mapping"], prediction[i] * 100))
+
+    # avoir l'index avec le plus d'occurences dans la prediction
+    predicted_index = np.argmax(prediction, axis=1)
+    music_genre = data["mapping"][predicted_index[0]]
+    print("Expected : {}, Predicted genre : {} || index : {}".format(y, music_genre, predicted_index))
+    return predicted_index, prediction
 
 
-"""
+def file_mfcc(file_path, num_samples_per_segment, expected_num_mfcc_vectors_per_segment,
+              dirpath=None,
+              data=data_empty,
+              hop_length=512, num_segments=5, n_mfcc=25, n_fft=2048,
+              iterator=1,
+              file_duration=30):
+    """ Extracts mfcc from audio file and saves it into a json file along with genre labels. """
+    signal, sample_rate = librosa.load(file_path, sr=sr)
 
-cookies format =
-	[ 'date',
-	['An old farm close to a little pound', 
-	[[' #FFC0CB', ' Pink', ' Joy', ' A feeling of happiness and excitement'], 
-	[' #F5F5DC', ' Beige', ' Calm', ' A feeling of peace and relaxation'], 
-	[' #FFFACD', ' Lemon Chiffon', ' Playfulness', ' A feeling of fun and amusement'], 
-	[' #ADD8E6', ' Light Blue', ' Creativity', ' A feeling of imagination and inventiveness'], 
-	[' #E0FFFF', ' Light Cyan', ' Freedom', ' A feeling of liberation and independence']]],
-	
-	['The desert of Sahara', 
-	[[' #007a7a', ' Pink', ' Joy', ' A feeling of happiness and excitement'], 
-	[' #f1d153', ' Beige', ' Calm', ' A feeling of peace and relaxation'], 
-	[' #8d0703', ' Lemon Chiffon', ' Playfulness', ' A feeling of fun and amusement'], 
-	[' #00ae00', ' Light Blue', ' Creativity', ' A feeling of imagination and inventiveness'], 
-	[' #efaa06', ' Light Cyan', ' Freedom', ' A feeling of liberation and independence']]],
+    if file_duration > 90:
+        # do not take the 60 first seconds
+        signal = signal[len(signal) // 2: len(signal) // 2 + 30 * sample_rate]  # :(60 + 30 * sample_rate)
 
-	['The moon', 
-	[' #1e3cab', ' Pink', ' Joy', ' A feeling of happiness and excitement'], 
-	[' #0c2b36', ' Beige', ' Calm', ' A feeling of peace and relaxation'], 
-	[' #dcdaee', ' Lemon Chiffon', ' Playfulness', ' A feeling of fun and amusement'], 
-	[' #b69264', ' Light Blue', ' Creativity', ' A feeling of imagination and inventiveness'], 
-	[' #5d0056', ' Light Cyan', ' Freedom', ' A feeling of liberation and independence']],
+    # process all segments of audio file
+    for segment in range(num_segments):
+        start_sample = num_samples_per_segment * segment
+        finish_sample = start_sample + num_samples_per_segment
 
-	]
+        # store the mfcc for segment if it has the expected length
+        mfcc = librosa.feature.mfcc(y=signal[start_sample:finish_sample],
+                                    sr=sample_rate,
+                                    n_fft=n_fft,
+                                    n_mfcc=n_mfcc,
+                                    hop_length=hop_length)
 
-colorsFormat = ['general prompt', [' #FFC0CB', ' Pink', ' Joy', ' A feeling of happiness and excitement'], +\
-	[' #F5F5DC', ' Beige', ' Calm', ' A feeling of peace and relaxation'], +\
-	[' #FFFACD', ' Lemon Chiffon', ' Playfulness', ' A feeling of fun and amusement'],  +\
-	[' #ADD8E6', ' Light Blue', ' Creativity', ' A feeling of imagination and inventiveness'],  +\
-	[' #E0FFFF', ' Light Cyan', ' Freedom', ' A feeling of liberation and independence']]
-	"""
+        mfcc = mfcc.T  # transpose the matrix
+        if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+            data["mfcc"].append(mfcc.tolist())
+            data["labels"].append(iterator - 1)
+            print("{}, segment:{}".format(file_path.split('\\')[-1], segment))
+
+
+def predict_sample(model, data, inputs):
+    stock = []
+    predictions = []
+    final_predictions = []
+    final_predictions2 = []
+    for i in range(len(inputs)):
+        x = inputs[i]
+        # X.reshape(132, 25, 1)
+        # print(inputs.shape)
+
+        predicted_index, prediction = predict(model, x, data)
+        predictions.append(100 * np.array(prediction[0]))
+        stock.append(predicted_index[0])
+
+    predicted_genre = data["mapping"][most_common(stock)]
+    final_predictions.append(np.mean(predictions, axis=0))
+    print(" ============ ", end="\n\n")
+    print("Predicted music genre is `{}`.".format(predicted_genre))
+    print(stock)
+    print(" ============ ", end="\n\n")
+    return predicted_genre
+
+
+# =============================================================================
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
